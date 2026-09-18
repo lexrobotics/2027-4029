ded7a4dc51a04d35bf7f1fac2e159bbe80cbb672
IDoSomeStuff <141585108+IDoSomeStuff09@users.noreply.github.com>
2026-09-18 21:18:15 +0300

    Limelight.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Sensors/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Sensors/Limelight.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Bot2.Bot;
 import org.firstinspires.ftc.teamcode.Bot2.Setup;
 
+@TeleOp
 public class Limelight extends LinearOpMode {
     private Setup setup;
     private Bot bot;
@@ -21,8 +22,8 @@ public class Limelight extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        setup = new Setup(hardwareMap, telemetry, true, this, Setup.OpModeType.AUTO, Setup.Team.Q1);
-        bot = new Bot(Setup.mechStates, Setup.sensorStates);
+//        setup = new Setup(hardwareMap, telemetry, true, this, Setup.OpModeType.AUTO, Setup.Team.Q1);
+//        bot = new Bot(Setup.mechStates, Setup.sensorStates);
 
         lime = hardwareMap.get(Limelight3A.class, "lime");
         lime.setPollRateHz(30);
