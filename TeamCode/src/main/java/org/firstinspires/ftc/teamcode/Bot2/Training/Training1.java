1ab64d491c1d1f51201914f00e3a3ed7abaece98
Comwewe <ripwiiu@gmail.com>
2026-09-24 16:47:03 -0400

    learning was changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Training/Training1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Training/Training1.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Bot2.Training;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp
 public class Training1 extends LinearOpMode {
 
