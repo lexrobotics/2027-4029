ee2d2bc6be497a207f76c2c6c31e7af50cc5c584
Comwewe <ripwiiu@gmail.com>
2026-09-24 16:56:38 -0400

    learning was changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Training/Training1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/Training/Training1.java
@@ -4,14 +4,23 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Bot2.Bot;
+import org.firstinspires.ftc.teamcode.Bot2.Setup;
 
 @TeleOp
 public class Training1 extends LinearOpMode {
 
+    private Setup setup;
+    private Bot bot;
+
     @Override
     public void runOpMode(){
+        setup = new Setup(hardwareMap, telemetry, true, this, Setup.OpModeType.AUTO, Setup.Team.Q1);
+        bot = new Bot(Setup.mechStates, Setup.sensorStates);
+
+
         waitForStart();
-        telemetry.addData("Hello, Team!");
+        telemetry.addData("Hello, Team!", "YES");
 
     }
 
